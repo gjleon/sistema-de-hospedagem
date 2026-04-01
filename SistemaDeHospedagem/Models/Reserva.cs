4118d169b3fbb3003e8856a7254d07a2daf16a1f
gjleon <gabriel_leon23@hotmail.com>
2026-04-01 12:25:47 -0400

    adicionando projeto

---
+++ SistemaDeHospedagem/Models/Reserva.cs
@@ -0,0 +1,51 @@
+namespace SistemaDeHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+
+            if (Suite.Capacidade >= hospedes.Count)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new InvalidOperationException("A capacidade da suíte é menor que o número de hóspedes.");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = Suite.ValorDiaria * DiasReservados;
+
+            if (DiasReservados >= 10)
+            {
+                valor = valor - (valor * 0.10m);
+            }
+
+            return valor;
+        }
+    }
+}
